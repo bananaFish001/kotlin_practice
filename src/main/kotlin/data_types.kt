330c6e33ca03d7c14d510a12ec48e3808ef24634
bananaFish001 <raj123.rs308@gmail.com>
2024-04-02 02:11:30 +0530

    when statement in kotlin is like switch statements in other languages

--- src/main/kotlin/data_types.kt
+++ src/main/kotlin/data_types.kt
@@ -3,4 +3,12 @@ fun main() {
     for (i in 1..10){
         println(inferredType)
     }
+    val x = 2
+    when (x) {
+        1 -> println("chupa chups cola flavour")
+        2 -> println("superpower")
+        else -> {
+            println("x is neither 1 and 2")
+        }
+    }
 }
